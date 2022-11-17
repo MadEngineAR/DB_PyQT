7fd6fcd08dc1fe60aa41dce73784f4318f757d8b
Mustaec R.F <scorpio-84@mail.ru>
2022-11-17 22:16:17 +0500

    reformat_to_OOP, some_rows_about_metaclass

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
+import dis
 import sys
 import json
 import socket
 import threading
 import time
+from pprint import pprint
+
 from common.variables import DEFAULT_IP_ADDRESS, DEFAULT_PORT
 from common.utils import get_message, send_message
 import logging
@@ -11,62 +14,149 @@ from errors import IncorrectDataRecivedError, ReqFieldMissingError, ServerError
 logger = logging.getLogger('client')
 
 
-def create_exit_message(account_name):
-    """Функция создаёт словарь с сообщением о выходе"""
-    return {
-        'action': 'exit',
-        'time': time.time(),
-        'user': {
-            "account_name": account_name,
-            "status": "Yep, I am here!"
-        },
-    }
+class ClientVerifier(type):
+    # Вызывается для создания экземпляра класса, перед вызовом __init__
+    def __init__(cls, future_class_name, future_class_parents, future_class_attrs):
+        """
+          Метод проверяет наличие атрибутов из списка required_attributes.
+          По умолчанию - ни один из обязательных атрибутов не найден
+          (изначально список not_found_attributes == required_attributes).
+        """
+        super().__init__(type)
+        # pprint(cls.__dict__)
+        # pprint(future_class_attrs)
+        # required_attributes = ['sock', 'account_name']
+        # not_found_attributes = required_attributes.copy()
+        # for attr, v in future_class_attrs.items():
+        #     if attr in required_attributes:
+        #         not_found_attributes.remove(attr)
+
+        # if not_found_attributes:
+        #     print(not_found_attributes)
+            # raise AttributeError(f"Not found attributes: {not_found_attributes}")
 
 
-def user_interactive(sock, username=None):
-    """Функция взаимодействия с пользователем, запрашивает команды, отправляет сообщения"""
-    print('Поддерживаемые команды:')
-    print('message - отправить сообщение. Кому и текст будет запрошены отдельно.')
-    print('help - вывести подсказки по командам')
-    print('exit - выход из программы')
+        # super(ClientVerifier, cls).__init__(future_class_name,
+        #                                     future_class_parents,
+        #                                     future_class_attrs)
+        # pprint(cls.__dict__)
+        for func in cls.__dict__:
+            try:
+                ret = dis.get_instructions(cls.__dict__[func])
+            except TypeError:
+                # print(func)
+                pass
+            else:
+                for i in ret:
+                    if i.argval in ['accept', 'listen']:
+                        raise ValueError(f'Недопуcтимый метод {i.argval}')
+                    pprint(i.argval)
 
-    while True:
-        command = input('Введите команду: ')
-        if command == 'message':
-            res = create_message(sock, username)
-            # print(res)
-            send_message(sock, res)
-        elif command == 'help':
-            # print_help()
-            print('Help yourself with yourself')
-        elif command == 'exit':
-            send_message(sock, create_exit_message(username))
-            print('Завершение соединения.')
-            logger.info('Завершение работы по команде пользователя.')
-            # Задержка неоходима, чтобы успело уйти сообщение о выходе
-            time.sleep(0.5)
-            break
-        else:
-            print('Команда не распознана, попробуйте снова. help - вывести поддерживаемые команды.')
+                    # print(i.argval)
 
 
-def make_presence(sock, login=None):
-    if not login:
-        login = input('Введите имя пользователя: ')
+class ClientSender(threading.Thread, metaclass=ClientVerifier):
+    def __init__(self, sock, account_name):
+        self.account_name = account_name
+        self.sock = sock
+        super().__init__()
 
+    def create_exit_message(self):
+        """Функция создаёт словарь с сообщением о выходе"""
+        return {
+            'action': 'exit',
+            'time': time.time(),
+            'user': {
+                "account_name": self.account_name,
+                "status": "Yep, I am here!"
+            },
+        }
+
+    def create_message(self):
+        """Функция запрашивает текст сообщения и возвращает его.
+        Так же завершает работу при вводе подобной комманды
+        """
+
+        message = input('Введите сообщение для отправки: ')
+        to = input('Введите получателя(-ей) сообщения: ')
+        message_dict = {
+            'action': 'message',
+            'time': time.time(),
+            'user': {
+                'account_name': self.account_name,
+                'sock': self.sock.getsockname(),
+            },
+            'to': to,
+            'message_text': message
+        }
+        logger.debug(f'Сформирован словарь сообщения: {message_dict}')
+        return message_dict
+
+    def user_interactive(self):
+        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        """Функция взаимодействия с пользователем, запрашивает команды, отправляет сообщения"""
+        print('Поддерживаемые команды:')
+        print('message - отправить сообщение. Кому и текст будет запрошены отдельно.')
+        print('help - вывести подсказки по командам')
+        print('exit - выход из программы')
+
+        while True:
+            command = input('Введите команду: ')
+            if command == 'message':
+                res = self.create_message()
+                send_message(self.sock, res)
+            elif command == 'help':
+                print('Help yourself with yourself')
+            elif command == 'exit':
+                send_message(self.sock, self.create_exit_message())
+                print('Завершение соединения.')
+                logger.info('Завершение работы по команде пользователя.')
+                # Задержка неоходима, чтобы успело уйти сообщение о выходе
+                time.sleep(0.5)
+                break
+            else:
+                print('Команда не распознана, попробуйте снова. help - вывести поддерживаемые команды.')
+
+
+def make_presence(sock, account_name):
+    if not account_name:
+        account_name = input('Введите имя пользователя: ')
     logger.debug('Сформировано сообщение серверу')
-
     # Генерация запроса о присутствии клиента
     data = {
         'action': 'presence',
         'time': time.time(),
         'type': 'status',
         'user': {
-            "account_name": login,
+            "account_name": account_name,
             "sock": sock.getsockname(),
         }
     }
     return data
+
+
+class ClientListener(threading.Thread):
+    def __init__(self, sock, account_name):
+        self.account_name = account_name
+        self.sock = sock
+        super().__init__()
+
+    def run(self):
+        while True:
+            try:
+                message = get_message(self.sock)
+                if 'response' in message:
+                    if message['response'] == 200 and message['data']:
+                        print(f'\nПолучено сообщение от клиента {message["login"]}\n {message["data"]}')
+                    logger.info('Bad request 400')
+                logger.info('Ошибка чтения данных')
+            except (IncorrectDataRecivedError, ValueError):
+                logger.error(f'Не удалось декодировать полученное сообщение.')
+            except (OSError, ConnectionError, ConnectionAbortedError,
+                    ConnectionResetError, json.JSONDecodeError):
+                logger.critical(f'Потеряно соединение с сервером.')
+                print('Потеряно соединение с сервером.')
+                break
 
 
 def response_process(sock):
@@ -81,45 +171,6 @@ def response_process(sock):
         logger.error('Ошибка чтения данных')
     except ValueError:
         print('Ошибка чтения данных')
-
-
-def message_from_server(sock, username=None):
-    while True:
-        try:
-            message = get_message(sock)
-            if 'response' in message:
-                if message['response'] == 200 and message['data']:
-                    print(f'\nПолучено сообщение от клиента {message["login"]}\n {message["data"]}')
-                logger.info('Bad request 400')
-            logger.info('Ошибка чтения данных')
-        except (IncorrectDataRecivedError, ValueError):
-            logger.error(f'Не удалось декодировать полученное сообщение.')
-        except (OSError, ConnectionError, ConnectionAbortedError,
-                ConnectionResetError, json.JSONDecodeError):
-            logger.critical(f'Потеряно соединение с сервером.')
-            print('Потеряно соединение с сервером.')
-            break
-
-
-def create_message(sock, login='Guest'):
-    """Функция запрашивает текст сообщения и возвращает его.
-    Так же завершает работу при вводе подобной комманды
-    """
-
-    message = input('Введите сообщение для отправки: ')
-    to = input('Введите получателя(-ей) сообщения: ')
-    message_dict = {
-        'action': 'message',
-        'time': time.time(),
-        'user': {
-            'account_name': login,
-            'sock': sock.getsockname(),
-        },
-        'to': to,
-        'message_text': message
-    }
-    logger.debug(f'Сформирован словарь сообщения: {message_dict}')
-    return message_dict
 
 
 def main_client():
@@ -140,7 +191,7 @@ def main_client():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((server_address, server_port))
-        send_message(s, make_presence(s, login=None))
+        send_message(s, make_presence(s, account_name=None))
         answer = response_process(s)
         logger.info(f'Установлено соединение с сервером. Ответ сервера: {answer["msg"]}')
         print(f'ИМЯ ПОЛЬЗОВАТЕЛЯ: {answer["login"]}')
@@ -162,12 +213,15 @@ def main_client():
         sys.exit(1)
     else:
 
-        receiver = threading.Thread(target=message_from_server, args=(s,))
+        # receiver = threading.Thread(target=message_from_server, args=(s,))
+        receiver = ClientListener(s, answer['login'])
         receiver.daemon = True
         receiver.start()
 
         # затем запускаем отправку сообщений и взаимодействие с пользователем.
-        user_interface = threading.Thread(target=user_interactive, args=(s, answer["login"]))
+        # user_interface = threading.Thread(target=user_interactive, args=(s, answer["login"]))
+        user_interface = ClientSender(s, answer['login'])
+        user_interface.user_interactive()
         user_interface.daemon = True
         user_interface.start()
         # user_interface.join()
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 """Программа Сервер. Используется идея:
 При регистрации на сервер, Имя Пользователя И адрес его Сокета записываются в слдоварь, и добавляются в
 глобальный список USERS. Отправка сообщения происходит при совпадении адреса Сокета и текущего Клиента"""
-
+import dis
 import socket
 import sys
+from pprint import pprint
+
 from my_socket import MySocket
 from select import select
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
@@ -15,56 +17,192 @@ logger = logging.getLogger('server')
 users = []
 
 
-def process_client_message(message):
-    global users
-    logger.debug(f'Получено сообщение от клиента {message}')
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME]:
-        msg = {RESPONSE: 200}
-        logger.info(f'Соединение с клиентом: НОРМАЛЬНОЕ {msg}')
-        if message[USER] not in users:
-            users.append(message[USER])
-        print(users)
+class ServerVerifier(type):
+    # Вызывается для создания экземпляра класса, перед вызовом __init__
+    def __init__(cls, future_class_name, future_class_parents, future_class_attrs):
+        """
+          Метод проверяет наличие атрибутов из списка required_attributes.
+          По умолчанию - ни один из обязательных атрибутов не найден
+          (изначально список not_found_attributes == required_attributes).
+        """
+        super().__init__(type)
+        # pprint(cls.__dict__)
+        pprint(future_class_attrs)
+        required_attributes = ['sock', 'account_name']
+        not_found_attributes = required_attributes.copy()
+        for attr, v in future_class_attrs.items():
+            if attr in required_attributes:
+                not_found_attributes.remove(attr)
+
+        if not_found_attributes:
+            print(not_found_attributes)
+            # raise AttributeError(f"Not found attributes: {not_found_attributes}")
+
+        # super(ClientVerifier, cls).__init__(future_class_name,
+        #                                     future_class_parents,
+        #                                     future_class_attrs)
+        pprint(cls.__dict__)
+        for func in cls.__dict__:
+            try:
+                ret = dis.get_instructions(cls.__dict__[func])
+            except TypeError:
+                print(func)
+                pass
+            else:
+                for i in ret:
+                    pprint(i.argval)
+                    # print(i)
+
+
+class Server(metaclass=ServerVerifier):
+    def __init__(self, listen_address, listen_port):
+        # Параметры подключения
+        self.addr = listen_address
+        self.port = listen_port
+        # Список подключенных клиентов
+        self.clients = []
+        self.clients_socket_names = []
+        # Список сообщений
+        self.messages = []
+
+    @staticmethod
+    def process_client_message(message):
+        global users
+        logger.debug(f'Получено сообщение от клиента {message}')
+        if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+                and USER in message and message[USER][ACCOUNT_NAME]:
+            msg = {RESPONSE: 200}
+            logger.info(f'Соединение с клиентом: НОРМАЛЬНОЕ {msg}')
+            if message[USER] not in users:
+                users.append(message[USER])
+            print(users)
+            return {
+                RESPONSE: 200,
+                'data': None,
+                'login': message['user']['account_name'],
+                'sock': message['user']['sock']
+            }
+        elif ACTION in message and message[ACTION] == 'message' and TIME in message \
+                and USER in message and message[USER][ACCOUNT_NAME]:
+            return {
+                RESPONSE: 200,
+                'data': message['message_text'],
+                'login': message['user']['account_name'],
+                'to': message['to'],
+                'sock_address': [user['sock'] for user in users if user['account_name'] == message['to']]
+            }
+        elif ACTION in message and message[ACTION] == 'exit' and TIME in message \
+                and USER in message and message[USER][ACCOUNT_NAME]:
+            return {RESPONSE: 200,
+                    'data': None,
+                    'login': message['user']['account_name'],
+                    'sock': message['user']['sock']
+                    }
+        msg = {
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
+        }
+        logger.error(f'Bad request 400', msg)
         return {
-            RESPONSE: 200,
-            'data': None,
-            'login': message['user']['account_name'],
-            'sock': message['user']['sock']
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
         }
-    elif ACTION in message and message[ACTION] == 'message' and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME]:
-        return {
-            RESPONSE: 200,
-            'data': message['message_text'],
-            'login': message['user']['account_name'],
-            'to': message['to'],
-            'sock_address': [user['sock'] for user in users if user['account_name'] == message['to']]
-        }
-    elif ACTION in message and message[ACTION] == 'exit' and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME]:
-        return  {RESPONSE: 200,
-            'data': None,
-            'login': message['user']['account_name'],
-            'sock': message['user']['sock']
-        }
-    msg = {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
-    logger.error(f'Bad request 400', msg)
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
+
+    def main_server(self):
+        """
+        Загрузка параметров командной строки, если нет параметров, то задаём значения по умолчанию.
+        Сначала обрабатываем порт:
+        server.py -p 8888 -a 127.0.0.1
+        """
+        logger.info(f'PORT : {self.port} ,IP_ADDRESS {self.addr}')
+        s = MySocket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((self.addr, self.port))
+        s.listen(MAX_CONNECTIONS)
+        s.settimeout(2)
+        # clients = []
+        # clients_socket_names = []
+        # messages = []
+        global users
+        while True:
+            try:
+                client, client_address = s.accept()
+            except OSError as e:
+                print(e.errno)
+            else:
+                self.clients.append(client)
+                self.clients_socket_names.append(client.getpeername())
+
+                print(self.clients_socket_names)
+            finally:
+                recv_data_lst = []
+                send_data_lst = []
+                # Проверяем на наличие ждущих клиентов
+                try:
+                    if self.clients:
+                        recv_data_lst, send_data_lst, err_lst = select(self.clients, self.clients, [], 0)
+                except OSError:
+                    pass
+
+                # принимаем сообщения и если ошибка, исключаем клиента.
+                if recv_data_lst:
+                    for client_with_message in recv_data_lst:
+                        try:
+                            message_from_client = get_message(client_with_message)
+                            if message_from_client['action'] == 'exit':
+                                client_with_message.close()
+                                recv_data_lst.remove(client_with_message)
+                                send_data_lst.remove(client_with_message)
+                            self.messages.append(message_from_client)
+                            self.process_client_message(message_from_client)
+                        except Exception:
+                            logger.info(f'Клиент {client_with_message.getpeername()} '
+                                        f'отключился от сервера.')
+                            client_with_message.close()
+                            # recv_data_lst.remove(client_with_message)
+                            # send_data_lst.remove(client_with_message)
+                            self.clients.remove(client_with_message)
+
+                # Если есть сообщения, обрабатываем каждое.
+                if send_data_lst and self.messages:
+                    for message in self.messages:
+                        print(message)
+                        self.messages.remove(message)
+                        for s_listener in send_data_lst:
+                            if s_listener.getpeername() in self.clients_socket_names \
+                                    and s_listener.getpeername() == tuple(message['user']['sock']) \
+                                    and message['action'] == 'presence':
+                                try:
+                                    response = self.process_client_message(message)
+                                    send_message(s_listener, response)
+                                except BrokenPipeError:
+                                    print('Вах')
+                                    send_data_lst.remove(s_listener)
+                                except KeyError:
+                                    self.clients.remove(s_listener)
+                                    pass
+                            elif s_listener.getpeername() in self.clients_socket_names and message[
+                                'action'] == 'message':
+                                try:
+                                    response = self.process_client_message(message)
+                                    print(response)
+
+                                    if len(response['sock_address']) == 0 \
+                                            and s_listener.getpeername() == tuple(message['user']['sock']):
+                                        response[
+                                            'data'] = f'Вы отправили сообщение не существующему адресату {message["to"]}'
+                                        send_message(s_listener, response)
+                                    elif s_listener.getpeername() == tuple(response['sock_address'][0]):
+                                        send_message(s_listener, response)
+                                        print('1111')
+                                except BrokenPipeError:
+                                    print('Вах')
+                                    send_data_lst.remove(s_listener)
+                                except IndexError:
+                                    pass
 
 
-def main_server():
-    """
-    Загрузка параметров командной строки, если нет параметров, то задаём значения по умолчанию.
-    Сначала обрабатываем порт:
-    server.py -p 8888 -a 127.0.0.1
-    """
-
+def start():
     try:
         if '-p' in sys.argv:
             listen_port = int(sys.argv[sys.argv.index('-p') + 1])
@@ -96,90 +234,9 @@ def main_server():
             'После параметра \'a\'- необходимо указать адрес')
         sys.exit(1)
 
-    logger.info(f'PORT : {listen_port} ,IP_ADDRESS {listen_address}')
-    s = MySocket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((listen_address, listen_port))
-    s.listen(MAX_CONNECTIONS)
-    s.settimeout(2)
-    clients = []
-    clients_socket_names = []
-    messages = []
-    global users
-    while True:
-        try:
-            client, client_address = s.accept()
-        except OSError as e:
-            print(e.errno)
-        else:
-            clients.append(client)
-            clients_socket_names.append(client.getpeername())
-
-            print(clients_socket_names)
-        finally:
-            recv_data_lst = []
-            send_data_lst = []
-            # Проверяем на наличие ждущих клиентов
-            try:
-                if clients:
-                    recv_data_lst, send_data_lst, err_lst = select(clients, clients, [], 0)
-            except OSError:
-                pass
-
-            # принимаем сообщения и если ошибка, исключаем клиента.
-            if recv_data_lst:
-                for client_with_message in recv_data_lst:
-                    try:
-                        message_from_client = get_message(client_with_message)
-                        if message_from_client['action'] == 'exit':
-                            client_with_message.close()
-                            recv_data_lst.remove(client_with_message)
-                            send_data_lst.remove(client_with_message)
-                        messages.append(message_from_client)
-                        process_client_message(message_from_client)
-                    except Exception:
-                        logger.info(f'Клиент {client_with_message.getpeername()} '
-                                    f'отключился от сервера.')
-                        client_with_message.close()
-                        # recv_data_lst.remove(client_with_message)
-                        # send_data_lst.remove(client_with_message)
-                        clients.remove(client_with_message)
-
-            # Если есть сообщения, обрабатываем каждое.
-            if send_data_lst and messages:
-                for message in messages:
-                    print(message)
-                    messages.remove(message)
-                    for s_listener in send_data_lst:
-                        if s_listener.getpeername() in clients_socket_names \
-                                and s_listener.getpeername() == tuple(message['user']['sock']) \
-                                and message['action'] == 'presence':
-                            try:
-                                response = process_client_message(message)
-                                send_message(s_listener, response)
-                            except BrokenPipeError:
-                                print('Вах')
-                                send_data_lst.remove(s_listener)
-                            except KeyError:
-                                clients.remove(s_listener)
-                                pass
-                        elif s_listener.getpeername() in clients_socket_names and message['action'] == 'message':
-                            try:
-                                response = process_client_message(message)
-
-                                if len(response['sock_address']) == 0 \
-                                        and s_listener.getpeername() == tuple(message['user']['sock']):
-                                    response[
-                                        'data'] = f'Вы отправили сообщение не существующему адресату {message["to"]}'
-                                    send_message(s_listener, response)
-                                elif s_listener.getpeername() == tuple(response['sock_address'][0]):
-                                    send_message(s_listener, response)
-                            except BrokenPipeError:
-                                print('Вах')
-                                send_data_lst.remove(s_listener)
-                            except IndexError:
-                                pass
+    server = Server(listen_address, listen_port)
+    server.main_server()
 
 
 if __name__ == '__main__':
-    main_server()
+    start()
