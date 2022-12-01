218fc4b4380a941465406ff5dfacf3997245ac8e
MadEngineAR <scorpio-84@mail.ru>
2022-12-01 08:52:20 +0500

    win_launch fixed

--- win_launch.py
+++ win_launch.py
@@ -12,18 +12,18 @@ while True:
     if ACTION == 'q':
         break
     elif ACTION == 's':
-        PROCESS.append(subprocess.Popen('python server.py',
+        try:
+            PROCESS.append(subprocess.Popen('python server.py', stdin=subprocess.PIPE,stdout=subprocess.PIPE,
                                         creationflags=subprocess.CREATE_NEW_CONSOLE))
-        # LISTENERS
-        #     for i in range(2):
-        #         sleep(1)
-        #         PROCESS.append(subprocess.Popen('python client.py',
-        #                                     creationflags=subprocess.CREATE_NEW_CONSOLE))
+        except Exception as e:
+            print(e)
 
-        # SENDERS
+
+        # Clients
         sleep(1)
+
         for i in range(2):
-            proc = subprocess.Popen('python client.py',
+            proc = subprocess.Popen('python client_PyQT.py',
                                             creationflags=subprocess.CREATE_NEW_CONSOLE)
             PROCESS.append(proc)
             print(PROCESS)
